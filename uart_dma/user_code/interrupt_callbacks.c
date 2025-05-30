736da7f27e3b50955a26868e955e7fe59bf94e7d
Matthieu Zimmermann <25462302+mattzimm91@users.noreply.github.com>
2025-05-30 09:50:35 +0200

    Ajouter projet "uart_dma"

---
+++ uart_dma/user_code/interrupt_callbacks.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include "interrupt_callbacks.h"
+
+extern UART_HandleTypeDef huart1;
+
+volatile uint8_t button_rising = 0U;
+volatile uint8_t button_falling = 0U;
+volatile uint8_t uart_rx_event = 0U;
+volatile uint8_t uart_tx_event = 0U;
+
+void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
+{
+	switch (GPIO_Pin) {
+		case BUTTON_Pin:
+			button_rising = 1U;
+			break;
+	}
+}
+
+void HAL_GPIO_EXTI_Falling_Callback(uint16_t GPIO_Pin)
+{
+	switch (GPIO_Pin) {
+		case BUTTON_Pin:
+			button_falling = 1U;
+			break;
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart == &huart1) {
+		uart_rx_event = 1U;
+	}
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart == &huart1) {
+		uart_tx_event = 1U;
+	}
+}
