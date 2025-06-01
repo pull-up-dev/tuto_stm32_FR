9f2651ffdd6b1cddd8a140ac7d40cc9ba8123455
Matthieu Zimmermann <25462302+mattzimm91@users.noreply.github.com>
2025-06-01 17:06:46 +0200

    Ajouter projet "i2c_blocking"

---
+++ i2c_blocking/user_code/interrupt_callbacks.h
@@ -0,0 +1,9 @@
+#ifndef INTERRUPT_CALLBACKS_H_
+#define INTERRUPT_CALLBACKS_H_
+
+#include <stdint.h>
+
+extern volatile uint8_t button_rising;
+extern volatile uint8_t button_falling;
+
+#endif /* INTERRUPT_CALLBACKS_H_ */
